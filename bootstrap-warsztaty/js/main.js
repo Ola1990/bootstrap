cf87533b033deed12d2763f6e101e769958ca298
Ola Goźlińska <aleksandra.gozlinska@wp.pl>
2018-07-08 16:55:14 +0200

    commit all

--- bootstrap-warsztaty/js/main.js
+++ bootstrap-warsztaty/js/main.js
@@ -1,1 +1,14 @@
 'use strict';
+$(function () {
+
+$(window).scroll(function(){
+   if($(document).scrollTop()>20){
+       $("#main-nav").css(
+       'background-color', 'rgba(14, 216, 214, 0.5)');
+   } else {
+      $("#main-nav").css(
+     'background-color', 'rgba(14, 216, 214, 0)' ); 
+   }
+});
+    
+});
